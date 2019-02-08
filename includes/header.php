142096878a00710b9d7e9e819687e9c410f72fca
Muhdo <gabrielmcv1@gmail.com>
2019-02-08 18:47:40 +0000

    novo header

--- includes/header.php
+++ includes/header.php
@@ -1,44 +1,49 @@
 <head>
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="css/header.css">
-   <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <header>
-   <nav class="topnav">
-      <div class="div-container">
-         <a class="link" href="index.php">
-            <img class="menu" src="img/logo.png">
-         </a>
+   <div class="div-nav-desktop">
+      <img src="img/logo.png">
+      <p class="name"><b>SOPAIS -</b><i> Componentes Metálicos Lda.</i></p>
+      <nav>
+         <a class="item" href="index.php">Inicio</a>
+         <a class="item" href="index.php">A Empresa</a>
+         <a class="item" href="index.php">O Que Fazemos</a>
+         <a class="item" href="index.php">Qualidade</a>
+         <a class="item" href="index.php">Notícias</a>
+         <a class="item" href="index.php">Contactos</a>
+      </nav>
+      <div class="lang">
+         <a href="index.php">PT</a>
+         <a href="index.php">EN</a>
       </div>
-      <div class="div-container">
-         <a class="link active" href="index.php">Inicio</a>
-      </div>
-      <div class="div-container">
-         <a class="link" href="quemsomos.php">Quem somos</a>
-      </div>
-      <div class="div-container">
-         <a class="link" href="index.php">Tecnologia</a>
-      </div>
-      <div class="div-container">
-         <a class="link" href="index.php">Produtos</a>
-      </div>
-      <div class="div-container">
-         <a class="link" href="index.php">Qualidade</a>
-      </div>
-      <div class="div-container">
-         <a class="link" href="index.php">Contactos</a>
-      </div>
-      <div class="div-container">
-         <a href="javascript:void(0);" class="icon" onclick="ClassChanger()">
-            <img class="icon" src="img/icons/menu.png">
-         </a>
-      </div>
-   </nav>
+   </div>
+   <div class="div-nav-mobile">
+      <img src="img/logo.png">
+      <a href="javascript:void(0);" class="icon" onclick="ClassChanger()">
+         <img class="icon" src="img/icons/menu.png">
+      </a>
+      <nav>
+         <a class="item" href="index.php">Inicio</a>
+         <a class="item" href="index.php">A Empresa</a>
+         <a class="item" href="index.php">O Que Fazemos</a>
+         <a class="item" href="index.php">Qualidade</a>
+         <a class="item" href="index.php">Notícias</a>
+         <a class="item" href="index.php">Contactos</a>
+
+         <div class="language">
+            <a class="itemLang langSel" href="index.php">PT</a>
+            <a class="itemLang" href="index.php">EN</a>
+         </div>
+      </nav>
+   </div>
 </header>
 <script>
+
 function ClassChanger() {
-   var x = $(".topnav");
+   var x = $("nav");
 
    if (x.hasClass("responsive")) {
       x.removeClass("responsive");
