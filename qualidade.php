1fc6d79bc4b24370e6b7170ba536aa615cdd844e
Muhdo <gabrielmcv1@gmail.com>
2019-02-22 17:37:19 +0000

    pagina qualidade. update style. update outras paginas

---
+++ qualidade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Sopais</title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/qualidade.css">
+   </head>
+   <body>
+      <?php
+         include_once "includes/header.php";
+      ?>
+      <main>
+         <img class="img-heading" src="https://i.imgur.com/ic9nTKH.jpg">
+         <div class="div-wrapper">
+            <div class="div-container">
+               <p>"A excelência com que a SOPAIS – Componentes Metálicos, Lda. serve os clientes sempre foi o seu maior foco.<br>
+                  Para cimentar a sua posição na qualidade de produção a empresa é certificada pelas normas de gestão IATF 16949:2016 e ISO 9001:2008.<br>
+                  Diminuir a pegada ecológica derivada da atividade industrial, e, assegurar um ambiente laboral saudável e seguro, são compromissos a que a Sopais – Componentes Metálicos, Lda. dedica bastante tempo e atenção, como se comprava com a obtenção das certificações de gestão da qualidade ambiental - ISO 14001:2015, e de gestão da segurança e saúde no trabalho - ISO 18001:2014."</p>
+            </div>
+
+            <div class="div-container">
+               <div class="div-box" id="color1">
+                  <img class="img-certif" src="img/certs/IATF16949-E.png">
+                  <h4>IATF 16949</h4>
+                  <div class="button">
+                     <a href="index.php">Download</a>
+                  </div>
+               </div>
+               <div class="div-box" id="color1">
+                  <img class="img-certif" src="img/certs/ISO9001Qu-E.png">
+                  <h4>ISO 9001</h4>
+                  <div class="button">
+                     <a href="index.php">Download</a>
+                  </div>
+               </div>
+               <div class="div-box" id="color2">
+                  <img class="img-certif" src="img/certs/ISO14001.png">
+                  <h4>ISO 14001</h4>
+                  <div class="button">
+                     <a href="index.php">Download</a>
+                  </div>
+               </div>
+               <div class="div-box" id="color3">
+                  <img class="img-certif" src="img/certs/NP4397_OHSAS18001.png">
+                  <h4>ISO 18001</h4>
+                  <div class="button">
+                     <a href="index.php">Download</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </main>
+      <?php
+         include_once "includes/footer.php";
+      ?>
+   </body>
+</html>
