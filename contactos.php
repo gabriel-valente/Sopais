74c3fa5f7c29c32a9db69673d84a02e9e476bd8a
Muhdo <gabrielmcv1@gmail.com>
2019-02-22 17:53:42 +0000

    Inicio Pagina Contactos

---
+++ contactos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Sopais</title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/contactos.css">
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
+      <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg==" crossorigin=""></script>
+   </head>
+   <body>
+      <?php
+         include_once "includes/header.php";
+      ?>
+      <main>
+         <img class="img-heading" src="https://i.imgur.com/ic9nTKH.jpg">
+         <div class="div-wrapper">
+             <div id="mapa"></div>
+         </div>
+      </main>
+
+      <script>
+         var mymap = L.map('mapa').setView([51.505, -0.09], 13);
+
+         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'your.mapbox.access.token'
+         }).addTo(mymap);
+      </script>
+      <?php
+         include_once "includes/footer.php";
+      ?>
+   </body>
+</html>
