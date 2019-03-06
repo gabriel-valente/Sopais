856e15f1c10edc0be21d325c93e195e95c948663
Muhdo <gabrielmcv1@gmail.com>
2019-03-06 21:14:28 +0000

    Pagina contactos. Correção Aside. update Header. Icons novos.

--- contactos.php
+++ contactos.php
@@ -6,8 +6,6 @@
       <title>Sopais</title>
       <link rel="stylesheet" href="css/style.css">
       <link rel="stylesheet" href="css/contactos.css">
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
-      <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg==" crossorigin=""></script>
    </head>
    <body>
       <?php
@@ -16,20 +14,41 @@
       <main>
          <img class="img-heading" src="https://i.imgur.com/ic9nTKH.jpg">
          <div class="div-wrapper">
-             <div id="mapa"></div>
+               <div class="mapouter">
+                  <div class="gmap_canvas">
+                     <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=40.775443%2C%20-8.590434&t=k&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no"></iframe>
+                  </div>
+               </div>
+            <div class="div-container">
+               <div class="div-content">
+                  <h6>Endereço</h6>
+                  <p>Eco-Parque Empresarial Estarreja<br>Rua de Canelas, nº3<br>3860-529 Estarreja, Portugal</p>
+               </div>
+               <div class="div-content">
+                  <h6>Fale Conosco</h6>
+                  <p>Tel.: (+351) 234 548 137<br>E-mail: geral@sopais.pt</p>
+               </div>
+            </div>
+            <div class="div-container">
+               <div class="div-content icons">
+                  <img src="img/icons/facebookbox.png">
+                  <img src="img/icons/youtubebox.png">
+                  <img src="img/icons/linkedinbox.png">
+               </div>
+               <div class="div-content">
+                  <form action="backend/enviar_contacto.php" method="post">
+                     <p>Nome:</p>
+                     <input type="text" name="nome" value="" placeholder="Nome" maxlength="60" required>
+                     <p>Email:</p>
+                     <input type="text" name="email" value="" placeholder="Email" maxlength="256" required>
+                     <p>Mensagem:</p>
+                     <textarea name="mensagem" rows="16" cols="80" placeholder="Mensagem" maxlength="512" required></textarea>
+                     <input type="submit" name="submit" value="Enviar">
+                  </form>
+               </div>
+            </div>
          </div>
       </main>
-
-      <script>
-         var mymap = L.map('mapa').setView([51.505, -0.09], 13);
-
-         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'your.mapbox.access.token'
-         }).addTo(mymap);
-      </script>
       <?php
          include_once "includes/footer.php";
       ?>
--- includes/header.php
+++ includes/header.php
@@ -5,72 +5,58 @@
    <script type="text/javascript" src="js/pageChange.js"></script>
 </head>
 <header>
-   <div class="div-nav-desktop">
-      <img src="img/logo.png">
-      <p class="name"><b>SOPAIS -</b><i> Componentes Metálicos Lda.</i></p>
-      <nav>
-         <a class="item" href="index.php">Inicio</a>
-         <a class="item" href="empresa.php">A Empresa</a>
-         <a class="item" href="quefazemos.php">O Que Fazemos</a>
-         <a class="item" href="qualidade.php">Qualidade</a>
-         <a class="item" href="noticias.php">Notícias</a>
-         <a class="item" href="contactos.php">Contactos</a>
-      </nav>
-      <div class="lang">
-         <a href="index.php">PT</a>
-         <a href="index.php">EN</a>
+   <img src="img/logo.png">
+   <p class="desktop name"><b>SOPAIS -</b><i> Componentes Metálicos Lda.</i></p>
+      <div class="mobile icon" id="nav-icon">
+         <span></span>
+         <span></span>
+         <span></span>
+         <span></span>
       </div>
-   </div>
-
-   <div class="div-nav-mobile">
-      <img src="img/logo.png">
-      <a href="javascript:void(0);" class="icon" id="noScript" onclick="ClassChanger()">
-         <img class="icon" src="img/icons/menu.png">
+   <div class="mobile div-social">
+      <a class="link" target="_blank" href="https://www.facebook.com/sopaislda" id="noScript">
+         <img class="social" src="img/icons/facebookbox.png">
       </a>
-      <div class="div-social">
-         <a class="link" target="_blank" href="https://www.facebook.com/sopaislda" id="noScript">
-            <img class="social" src="img/icons/facebook.png">
-         </a>
-         <a class="link" target="_blank" href="https://www.youtube.com/channel/UCUwKckOoooyihJaUKwSuMIQ" id="noScript">
-            <img class="social" src="img/icons/youtube.png">
-         </a>
-         <a class="link" target="_blank" href="https://www.linkedin.com/company/sopais?trk=tyah&trkInfo=tarId%3A1397663293041%2Ctas%3Asopais%2Cidx%3A1-1-1" id="noScript">
-            <img class="social" src="img/icons/linkedin.png">
-         </a>
-      </div>
-      <nav>
-         <a class="item" href="index.php">Inicio</a>
-         <a class="item" href="index.php">A Empresa</a>
-         <a class="item" href="index.php">O Que Fazemos</a>
-         <a class="item" href="index.php">Qualidade</a>
-         <a class="item" href="index.php">Notícias</a>
-         <a class="item" href="index.php">Contactos</a>
+      <a class="link" target="_blank" href="https://www.youtube.com/channel/UCUwKckOoooyihJaUKwSuMIQ" id="noScript">
+         <img class="social" src="img/icons/youtubebox.png">
+      </a>
+      <a class="link" target="_blank" href="https://www.linkedin.com/company/sopais?trk=tyah&trkInfo=tarId%3A1397663293041%2Ctas%3Asopais%2Cidx%3A1-1-1" id="noScript">
+         <img class="social" src="img/icons/linkedinbox.png">
+      </a>
+   </div>
+   <nav>
+      <a class="item" href="index.php">Inicio</a>
+      <a class="item" href="empresa.php">A Empresa</a>
+      <a class="item" href="quefazemos.php">O Que Fazemos</a>
+      <a class="item" href="qualidade.php">Qualidade</a>
+      <a class="item" href="noticias.php">Notícias</a>
+      <a class="item" href="contactos.php">Contactos</a>
 
-         <div class="language">
-            <a class="itemLang langSel" href="index.php">PT</a>
-            <a class="itemLang" href="index.php">EN</a>
-         </div>
-      </nav>
+      <div class="mobile language">
+         <a class="itemLang langSel" href="index.php">PT</a>
+         <a class="itemLang" href="index.php">EN</a>
+      </div>
+   </nav>
+   <div class="desktop lang">
+      <a href="index.php">PT</a>
+      <a href="index.php">EN</a>
    </div>
 </header>
 <script>
 
-$(window).on("scroll", function () {
-   if ($(this).scrollTop() > 0) {
-      $("header").addClass("minimize");
-   }
-   else {
-      $("header").removeClass("minimize");
-   }
+$(document).ready(function(){
+   $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 0) {
+         $("header").addClass("minimize");
+      }
+      else {
+         $("header").removeClass("minimize");
+      }
+   });
+
+	$("#nav-icon").click(function(){
+      $(this).toggleClass("open");
+		$("nav").toggleClass("responsive");
+	});
 });
-
-function ClassChanger() {
-   var x = $("nav");
-
-   if (x.hasClass("responsive")) {
-      x.removeClass("responsive");
-   } else {
-      x.addClass("responsive");
-   }
-}
 </script>
